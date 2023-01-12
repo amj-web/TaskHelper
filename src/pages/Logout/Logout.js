3ef47b3093477be179ee769867f531bbb0444337
amj-web <andrewmjarrett@gmail.com>
2023-01-12 15:29:16 +0500

    Logout functionality completed

---
+++ src/pages/Logout/Logout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Logout.css"
+const LogOut = () => {
+    return (
+        <React.Fragment>
+            <div className="logout-page">
+                <h1>You have logged out!!</h1>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default LogOut;
