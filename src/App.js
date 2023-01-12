a18a18732dc3d554f3c6d4aa082128810f95eefb
amj-web <andrewmjarrett@gmail.com>
2023-01-12 15:32:09 +0500

    Application global css added and application logic is completed - all urls are done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useEffect,useState}from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Signup from "./pages/SignUp/Signup";
+import Login from "./pages/Login/Login";
+import Category from "./pages/Category/Category";
+import LogOut from "./pages/Logout/Logout";
+import Todo from "./pages/ToDo/ToDo";
+import Docs from "./pages/Docs/Docs";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import Update from "./pages/Update/Update";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const userData = JSON.parse(localStorage.getItem('user'))
+    const [isLogin, setIsLogin] = useState()
+    useEffect(() => {
+        // console.log("This is user Data",userData)
+        if (userData == null) {
+            setIsLogin(false)
+          } else {
+            setIsLogin(true)
+          }
+    }, [])
+    
+    return (
+        <Router>
+            <div className="App">
+                <Navbar isLogin={isLogin} setIsLogin={setIsLogin}/>
+                <div className="todos-body">
+                <Switch >
+                    <Route path="/" exact component={Home} />
+                    <Route path="/categories" component={Category} />
+                    <Route path="/todo" component={Todo} />
+                    <Route path="/update" component={Update} />
+                    <Route path="/docs" component={Docs} />
+                    <Route path="/signUp" component={Signup} />
+                    <Route path="/logout" exact component={LogOut} />
+                    <Route path="/login" component={(props) => <Login setIsLogin={setIsLogin} {...props} />}  />
+                </Switch>
+                </div>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
